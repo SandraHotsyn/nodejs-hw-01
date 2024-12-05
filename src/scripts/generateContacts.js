26a82c59a35e157ae93a6ede08db8ed0beded16b
Hotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-12-05 03:52:26 +0200

    add const abs path

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import * as fs from 'node:fs';
+
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { error, log } from 'node:console';
+
+const generateContacts = async (number) => {
+  const constantsList = Array(number).fill(0).map(createFakeContact);
+  fs.readFile('../../db/db.json', (error, data) => {
+    console.log(error);
+    console.log(data);
+  });
+};
 
 generateContacts(5);
